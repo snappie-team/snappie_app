486cc6dd4580cca57d419a6c2b0a4808e1c9afc0
tafifa <tafifa09@gmail.com>
2026-01-15 00:16:16 +0700

    chore: update app version to 1.2.0 and increment build number to 5

--- lib/app/core/constants/app_constants.dart
+++ lib/app/core/constants/app_constants.dart
@@ -16,7 +16,7 @@ class AppConstants {
   
   // App Info
   static const String appName = 'Snappie App';
-  static const String appVersion = '1.1.3';
+  static const String appVersion = '1.2.0';
   static const int appVersionCode = 4;
   static const String appDescription = 'Discover and explore amazing places';
   

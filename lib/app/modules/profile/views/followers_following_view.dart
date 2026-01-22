1e9c1d35291bab37473e903f47b011a768ed4c29
tafifa <tafifa09@gmail.com>
2026-01-22 18:13:14 +0700

    refactor(profile): replace custom search icon with material icon
    Use standard Material Icons.search instead of custom AppIcon for consistency with Flutter's design system and to simplify the codebase by removing unused asset imports.

--- lib/app/modules/profile/views/followers_following_view.dart
+++ lib/app/modules/profile/views/followers_following_view.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:snappie_app/app/modules/shared/layout/views/scaffold_frame.dart';
 import '../../../core/constants/app_colors.dart';
-import '../../../core/constants/app_assets.dart';
 import '../../../core/services/logger_service.dart';
 import '../../../core/helpers/error_handler.dart';
 import '../../../data/models/social_model.dart';
 import '../../../data/repositories/social_repository_impl.dart';
 import '../../../routes/app_pages.dart';
 import '../../shared/widgets/index.dart';
-import '../../shared/widgets/_display_widgets/app_icon.dart';
 
 /// View type enum
 enum FollowViewType { followers, following }
@@ -121,7 +119,10 @@ class _FollowersFollowingViewState extends State<FollowersFollowingView> {
         decoration: InputDecoration(
           hintText: 'Cari',
           hintStyle: TextStyle(color: AppColors.textSecondary),
-          prefixIcon: AppIcon(AppAssets.icons.search, color: AppColors.textSecondary, size: 24),
+          prefixIcon: Icon(
+            Icons.search,
+            color: AppColors.textSecondary,
+          ),
           suffixIcon: _searchQuery.isNotEmpty
               ? IconButton(
                   // TODO: Add clear.svg icon to assets/icons/
